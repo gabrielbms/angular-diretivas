b3dc24c6d95441ba11879af1ff3b66f7684174cd
Gabriel Bueno <gabriel.bueno.m.s@hotmail.com.br>
2022-01-20 08:16:50 -0300

    #29: Diretivas: ngClass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiretivaNgifComponent
+    DiretivaNgifComponent,
+    DiretivaNgclassComponent
   ],
   imports: [
     BrowserModule
